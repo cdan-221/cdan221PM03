baf3c6d51fcbd2f8aba8d360d0d5398bb16fcf2f
ting2122 <tchang@massart.edu>
2020-10-14 21:16:51 +0800

    connect scene 7, 8, 9

---
+++ Branching Narrative/Assets/Scripts/DialogueScene8.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class DialogueScene8 : MonoBehaviour {
+        public int primeInt = 1; // This integer drives game progress!
+        public Text Char1name;
+        public Text Char1speech;
+        public Text Char2name;
+        public Text Char2speech;
+        public Text Char3name;
+        public Text Char3speech;
+        public GameObject dialogue;
+        public GameObject ArtChar1;
+        public GameObject ArtBG1;
+        public GameObject Choice1a;
+        public GameObject Choice1b;
+        public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
+        public GameObject nextButton;
+       //public GameObject gameHandler;
+       //public AudioSource audioSource;
+        private bool allowSpace = true;
+
+void Start(){         // initial visibility settings
+        dialogue.SetActive(false);
+        ArtChar1.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+   }
+
+void Update(){         // use spacebar as Next button
+        if (allowSpace == true){
+                if (Input.GetKeyDown("space")){
+                       talking();
+                }
+        }
+   }
+
+public void talking(){         // main story function. Players hit next to progress to next int
+        primeInt = primeInt + 1;
+        if (primeInt == 1){
+                // AudioSource.Play();
+        }
+        else if (primeInt == 2){
+               ArtChar1.SetActive(true);
+                dialogue.SetActive(true);
+                Char1name.text = "Narrator";
+                Char1speech.text = "You talk with your bf if you wanna either go cook a healthy dinner or go out to eat";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt ==3){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "What do you feel like eating for dinner tonight?";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 4){
+                Char1name.text = "You";
+                Char1speech.text = " I don't know?";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 5){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "what are you in the mood for?";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 6){
+                Char1name.text = "You";
+                Char1speech.text = "…looking at your phone…";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt ==7){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "Maybe Chinese food?";
+        }
+        else if (primeInt ==8){
+                Char1name.text = "You";
+                Char1speech.text = "hhmmmm…";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt ==9){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "Mcdonalds?";
+        }
+        else if (primeInt ==10){
+                Char1name.text = "You";
+                Char1speech.text = "Anything, but mcdonalds please.";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 11){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "Do you feel like going to stop and shop and getting healthy food? or going to TGIF Fridays, even though it's not friday? LOL";
+                // Turn off "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice1a.SetActive(true); // function Choice1aFunct()
+                Choice1b.SetActive(true); // function Choice1bFunct()
+        }
+// ENCOUNTER AFTER CHOICE #1
+       else if (primeInt == 100){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "OK";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+        }
+       else if (primeInt == 101){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+
+       else if (primeInt == 200){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = "hmm";               
+        }
+       else if (primeInt == 201){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Christoff";
+                Char2speech.text = " I guess I’ll just get a cup of water then. Lol";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene2Button.SetActive(true);
+        }
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
+        public void Choice1aFunct(){
+                Char1name.text = "You";
+                Char1speech.text = "Lets go to stop and shop, so I could save money and eat something healthy.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 99;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice1bFunct(){
+                Char1name.text = "You";
+                Char1speech.text = "Nice idea! I haven't had their steak in while! And their lava cake ice cream. YUM";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 199;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void SceneChange2a(){
+               SceneManager.LoadScene("Scene10");
+        }
+        public void SceneChange2b(){
+                SceneManager.LoadScene("Scene10");
+        }
+}
